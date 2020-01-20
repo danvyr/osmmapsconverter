8930ae385a90a2df38656d65155fcd784fdbbb30
Vitali Danilovich <danvyr@gmail.com>
2020-01-20 16:58:16 +0300

    rename poly, downoload and split works

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ https://wiki.openstreetmap.org/wiki/OsmAndMapCreator
 
 ## split
 https://wiki.openstreetmap.org/wiki/Osmconvert#Clipping_based_on_a_Polygon
+
 apt install osmctools
 
 https://wiki.openstreetmap.org/wiki/Mkgmap/help/splitter
@@ -26,10 +27,19 @@ https://wiki.openstreetmap.org/wiki/Mkgmap/help/splitter
 ## Belarus
 
 http://download.geofabrik.de/europe/belarus-latest.osm.pbf
+
 http://polygons.openstreetmap.fr/?id=59506
+
 Brest   https://www.openstreetmap.org/relation/59189
+
 Gomel   https://www.openstreetmap.org/relation/59161
+
 Grodno  https://www.openstreetmap.org/relation/59275
+
 Minsk   https://www.openstreetmap.org/relation/59752
+
+Minsk City https://www.openstreetmap.org/relation/59195
+
 Mogilev https://www.openstreetmap.org/relation/59162
+
 Vitebsk https://www.openstreetmap.org/relation/59506
--- osmMapCreator.py
+++ osmMapCreator.py
@@ -6,14 +6,18 @@
 #TODO сделать запуск в докере
 #TODO выбор какие карты и чем собирать с какими стилями и как резать.
 #TODO текстовый гуи интерфейс для 
-#TODO 
+#TODO сделать тестовую замену name на name:be
 
 import sys
 import requests
 import urllib.request
+import os
+from os import listdir
+from os.path import isfile, join
+from os import walk
 
 
-in_dir = 'in'
+
 urls = {
     'osmandcreator': 'https://download.osmand.net/latest-night-build/OsmAndMapCreator-main.zip',
     'maps': 
@@ -21,8 +25,59 @@ urls = {
         
         }
 }
+polyDir = 'poly'
+inputDir = 'in'
+splitDir = 'split'
+
+currentMap = 'currentMap.txt'
 
 
+def checkVerstion():
+    #наверное не надо
+    version = ''
+    try:
+        with open(currentMap, 'r') as cf:
+            version = cf.readline()
+    except:
+        with open(currentMap, 'w') as cf:
+            cf.write(version)
+    return 1
+    
+
+
+def download():
+    print ('Start downloading')
+    try:
+        for map_name, url_to_map in urls['maps'].items():
+            print(map_name)
+            resp = requests.head(url_to_map)        
+            print("Last modified: " + resp.headers['last-modified'])  
+            #pathToFile = os.path.join(inputDir,  map_name + '.osm.pbf')   # не работает     
+            pathToFile = inputDir + '/' + map_name + '.osm.pbf'
+            print (pathToFile)
+            urllib.request.urlretrieve(url_to_map,  pathToFile)            
+            print ('all downloaded')
+            return 1
+    except:
+        return 0
+
+
+def split():
+    try:
+        for mapFile in os.listdir(inputDir):
+            print(mapFile)
+            for polyFile in os.listdir(polyDir):
+                print(polyFile)
+                cmd= 'osmconvert ' + inputDir +'/' + mapFile +' -B=' + polyDir +'/' + polyFile + ' --complete-ways --complex-ways -o='+ splitDir + '/' + polyFile.replace('poly','pbf') +' --statistics'
+                print (cmd)
+                os.system(cmd)
+        return 1
+    except OSError as err:
+        print("OS error: {0}".format(err))
+    except ValueError:
+        print("Could not convert data to an integer.")
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
 
 def osmand():
     pass
@@ -30,32 +85,15 @@ def osmand():
 def mapsme():
     pass
 
-def split():
-    return 1
-
-def download():
-    for map_name, url_to_map in urls['maps']:
-        resp = requests.head(url_to_map)        
-        print("Last modified: " + resp.headers['last-modified'])
-
-
-        urllib.request.urlretrieve(url_to_map, in_dir + map_name)
-        return 1
 
 
 def main():
-    if download():
-        mapsme()
-        if split():
-            osmand()
+    #if download():
+    #     mapsme()
+    #     if split():
+    #         osmand()
         
-
-
-
-
-
-    
-
+    split()
 
 if __name__ == '__main__':
     main()
