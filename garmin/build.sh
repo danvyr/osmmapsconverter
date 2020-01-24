140c1434466c1a22b0fe04494f6f4fd707634d06
Vitali Danilovich <danvyr@gmail.com>
2020-01-24 15:28:13 +0300

    add garmin and mapsme

---
+++ garmin/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#prepare
+wget http://www.mkgmap.org.uk/download/mkgmap-r4425.tar.gz
+
+mkdir mkgmap
+tar -xf mkgmap-r4425.tar.gz --directory mkgmap
+mv mkgmap/mkgmap-r4425/* mkgmap
+rmdir mkgmap/mkgmap-r4425
+
+rmdir split
+mkdir split
+
+wget http://www.mkgmap.org.uk/download/splitter-r595.tar.gz
+
+tar -xf splitter-r595.tar.gz  --directory  split
+mv split/splitter-r595/* split
+rmdir split/splitter-r595
+
+#build map
+java -jar splitter.jar --output-dir=temp ../in/belarus.osm.pbf 
+
+java -jar mkgmap/mkgmap.jar   --route --add-pois-to-areas --bounds=bounds --index  --gmapsupp --mapname=80808080 --country-name=Belarus --country-abbr=BY   --output-dir  ../out
+
+
