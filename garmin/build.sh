0d81323ba8e8bef3cde4e54c8960ad21059fee7c
Vitali Danilovich <danvyr@gmail.com>
2020-01-27 11:19:47 +0100

    pre final#2

--- garmin/build.sh
+++ garmin/build.sh
@@ -5,7 +5,7 @@
 #build map
 
 
-#java -jar split/splitter.jar --output-dir=temp ../in/belarus.osm.pbf
+java -jar split/splitter.jar --output-dir=temp ../in/belarus.osm.pbf
 
 cd temp
 java -jar ../mkgmap/mkgmap.jar --route --add-pois-to-areas --bounds=bounds --index  --gmapsupp --mapname=80808080  --link-pois-to-ways  --country-name=Belarus --country-abbr=BY  6324*.osm.pbf
