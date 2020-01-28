d114f70788efbccd8d066b598548bd36b953f4ab
Vitali Danilovich <danvyr@gmail.com>
2020-01-28 18:12:04 +0300

    refactor#1

--- osmMapCreator.py
+++ osmMapCreator.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3.6
 
-#TODO структура папки out
-#TODO доставание полигонов из osm для любой страны (по админ уровню)
-#TODO map rotate
-#TODO сделать запуск локальный  
-#TODO сделать запуск в докере
-#TODO выбор какие карты и чем собирать с какими стилями и как резать.
-#TODO текстовый гуи интерфейс для 
-#TODO генерация json для приложения загрузки на android 
-#TODO сделать тестовую замену name на name:be
+# TODO доставание полигонов из osm для любой страны (по админ уровню)
+# TODO выбор какие карты и чем собирать с какими стилями и как резать.
+# TODO генерация json для приложения загрузки на android
+# TODO сделать тестовую замену name на name:be
 
 import sys
 import requests
@@ -17,17 +12,16 @@ import os
 import shutil
 
 
-
-
-
 urls = {
     'osmandcreator': 'https://download.osmand.net/latest-night-build/OsmAndMapCreator-main.zip',
-    'maps': 
+    'maps':
         {'belarus': 'http://download.geofabrik.de/europe/belarus-latest.osm.pbf',
-        
-        }
+
+         }
 }
-currentDir = os.path.abspath( '.' )
+
+user = 'osm'
+currentDir = os.path.abspath('.')
 
 currentMap = 'currentMap.txt'
 
@@ -35,24 +29,30 @@ inputDir = os.path.abspath('in')
 
 polyDir = os.path.abspath('poly')
 splitDir = os.path.abspath('split')
+
 mapsmeDir = os.path.abspath('mapsme')
-
 osmandDir = os.path.abspath('osmand')
-OAMCDir  = osmandDir + '/OsmAndMapCreator'
-
 garminDir = os.path.abspath('garmin')
 
-tempGarmin = os.path.abspath('garmin/temp')
-tempMapsme = os.path.abspath('mapsme/out')
+OAMCDir = osmandDir + '/OsmAndMapCreator'
+
+tempGarmin = garminDir + '/temp'
+tempMapsme = mapsmeDir + '/temp'
 
 outDir = os.path.abspath('/var/www/maps')
 outOsmAnd = outDir + '/osmand'
 outMapsme = outDir + '/mapsme'
 outGarmin = outDir + '/garmin'
 
+tempDirs = [inputDir, tempMapsme, tempGarmin]
+
+innerDirs = [polyDir, splitDir, mapsmeDir, osmandDir,
+             garminDir, OAMCDir, tempGarmin, tempMapsme]
+outDirs = [outDir, outOsmAnd, outMapsme, outGarmin]
+
 
 def checkVerstion():
-    #наверное не надо
+    # наверное не надо
     version = ''
     try:
         with open(currentMap, 'r') as cf:
@@ -63,8 +63,30 @@ def checkVerstion():
 
     return 1
 
+
+def checkDirs():
+    for folder in innerDirs:
+        if not os.path.isdir(folder):
+            try:
+                os.mkdir(folder)
+            except:
+                os.system('sudo mkdir -p ' + folder)
+                os.system('sudo chown ' + user + ':' + user + ' ' + folder ) 
+
+    for folder in outDirs:
+        if not os.path.isdir(folder):
+            try:
+                os.mkdir(folder)
+            except:
+                os.system('sudo mkdir -p ' + folder)
+                os.system('sudo chown ' + user + ':' + user + ' ' + folder ) 
+
+
+
 def prepare():
-    #install
+    checkDirs()
+
+    # install
     try:
         print('install osmctools')
         os.system('sudo apt-get install osmctools')
@@ -82,29 +104,35 @@ def prepare():
         os.system('./tools/unix/build_omim.sh -sr generator_tool')
         os.chdir('tools/python/maps_generator')
         os.system('pip3 install -r requirements.txt')
-        os.system('cp ' + mapsmeDir + '/map_generator.ini var/etc/map_generator.ini')
+        os.system('cp ' + os.path.join(mapsmeDir,'/map_generator.ini') + ' var/etc/map_generator.ini')
         os.chdir(currentDir)
 
     except:
         pass
 
-
-    #prepare OSMAND
+    # prepare OSMAND
     try:
         print('install osmand')
 
         url = urls['osmandcreator']
         resp = requests.head(url)
         print("Last modified: " + resp.headers['last-modified'])
-        pathToFile = osmandDir + '/' + 'OsmAndMapCreator-main.zip'
+        pathToFile = os.path.join(osmandDir, 'OsmAndMapCreator-main.zip')
         urllib.request.urlretrieve(url,  pathToFile)
-        os.system('unzip '+  pathToFile + ' -d ' + OAMCDir )
+        os.system('unzip ' + pathToFile + ' -d ' + OAMCDir)
     except:
         pass
 
+    # prepare Garmin
+    os.chdir(garminDir)
+    os.system('bash prepare.sh')
+    os.chdir(currentDir)
+
+
 def clean():
-    print ([inputDir, tempMapsme, tempGarmin])
-    for folder in [inputDir, tempMapsme, tempGarmin]:
+
+    print(tempDirs)
+    for folder in tempDirs:
         print(folder)
         for filename in os.listdir(folder):
             file_path = os.path.join(folder, filename)
@@ -116,89 +144,89 @@ def clean():
             except Exception as e:
                 print('Failed to delete %s. Reason: %s' % (file_path, e))
 
+
 def move():
 
-    #check files in out folders and backup
+    # check files in out folders and backup
 
-    #move OsmAnd
-    print ('move OsmAND map')
+    # move OsmAnd
+    print('move OsmAND map')
     for file in os.listdir(osmandDir):
         if file.endswith('.obf'):
             print(file)
-            shutil.move(os.path.join(osmandDir, file), os.path.join(outOsmAnd, file))
+            shutil.move(os.path.join(osmandDir, file),
+                        os.path.join(outOsmAnd, file))
 
-    #move mapsme
-    tmpMapsme = mapsmeDir + '/out'
+    # move mapsme
+    # find mapme out
     path = ''
     status = False
-    for folder in os.listdir(tmpMapsme):
-#        print (folder)
-        if ( folder.find('20') > -1) and os.path.isdir(tmpMapsme + '/' + folder):
-            d = tmpMapsme + '/' + folder
-            print( folder)
-            for t in os.listdir(d):
-                g = d + '/' + t
-                if (t.find('20') > -1) and os.path.isdir(g):
-                    os.chdir(g)
-                    with open(d+'/status/stages.status','r') as f:
+    for folder in os.listdir(tempMapsme):
+        dirL1 = os.path.join(tempMapsme , folder)        
+        if (folder.find('20') > -1) and os.path.isdir(dirL1):            
+            print(dirL1)
+            for t in os.listdir(dirL1):
+                dirL2 = os.path.join(dirL1, t)
+                if (t.find('20') > -1) and os.path.isdir(dirL2):
+                    os.chdir(dirL2)
+                    with open(os.path.join( dirL1, 'status/stages.status'), 'r') as f:
                         str = f.readline()
-                        print (str)
+                        print(str)
                         if str == 'finish':
-                             status = True
-                             path =  g
-                             print (path)
+                            status = True
+                            path = dirL2
+                            print(path)
             os.chdir(outMapsme)
-#        except:
-#            pass
 
-    print ('move mapsme map' ) 
+    # moving mapsme out
+    print('move mapsme map')
     if status:
         for file in os.listdir(path):
             if file.endswith('.mwm'):
                 print(file)
                 shutil.move(os.path.join(path, file),
-                        os.path.join(outMapsme, file))
+                            os.path.join(outMapsme, file))
     os.chdir(currentDir)
 
-    
-
-
-    #move garmin
-    print ('move garmin map')
+    # move garmin
+    print('move garmin map')
     try:
-        shutil.move(os.path.join(garminDir+'/temp', 'gmapsupp.img'),
-                os.path.join(outGarmin, 'gmapsupp.img'))
+        shutil.move(os.path.join(garminDir, 'temp', 'gmapsupp.img'),
+                    os.path.join(outGarmin, 'gmapsupp.img'))
     except:
-         print ('no garmin map')
+        print('no garmin map')
+
 
 def download():
-    print ('Start downloading maps')
+
+    print('Start downloading maps')
+
     try:
         for map_name, url_to_map in urls['maps'].items():
             print(map_name)
-            resp = requests.head(url_to_map)        
-            print("Last modified: " + resp.headers['last-modified'])  
-            #pathToFile = os.path.join(inputDir,  map_name + '.osm.pbf')   # не работает     
+            resp = requests.head(url_to_map)
+            print("Last modified: " + resp.headers['last-modified'])
+            # pathToFile = os.path.join(inputDir,  map_name + '.osm.pbf')   # не работает
             pathToFile = os.path.join(inputDir, map_name + '.osm.pbf')
-            print (pathToFile)
-            urllib.request.urlretrieve(url_to_map,  pathToFile)            
-            print ('all downloaded')
+            print(pathToFile)
+            urllib.request.urlretrieve(url_to_map,  pathToFile)
+            print('all downloaded')
+        return 1
 
-
-
-        return 1            
     except:
         return 0
 
 
 def split():
+
     try:
         for mapFile in os.listdir(inputDir):
             print(mapFile)
             for polyFile in os.listdir(polyDir):
                 print(polyFile)
-                cmd= 'osmconvert ' + os.path.join(inputDir, mapFile) +' -B=' + os.path.join(polyDir, polyFile) + ' --complete-ways --complex-ways -o='+ os.path.join(splitDir, polyFile.replace('poly','pbf') ) +' --statistics'
-                print (cmd)
+                cmd = 'osmconvert ' + os.path.join(inputDir, mapFile) + ' -B=' + os.path.join(
+                    polyDir, polyFile) + ' --complete-ways --complex-ways -o=' + os.path.join(splitDir, polyFile.replace('poly', 'pbf')) + ' --statistics'
+                print(cmd)
                 os.system(cmd)
         return 1
     except OSError as err:
@@ -211,12 +239,13 @@ def split():
         print("Unexpected error:", sys.exc_info()[0])
         return 0
 
+
 def osmand():
     try:
         os.chdir(osmandDir)
         #folder = 'OsmAndMapCreator'
         for mapFile in os.listdir(splitDir):
-            mapFile = os.path.join(splitDir, mapFile )
+            mapFile = os.path.join(splitDir, mapFile)
             print(mapFile)
 
             cmd = 'java -Djava.util.logging.config.file="'+OAMCDir+'/logging.properties" \
@@ -224,7 +253,7 @@ def osmand():
                 -cp "'+OAMCDir+'/OsmAndMapCreator.jar:'+OAMCDir+'/lib/*.jar" net.osmand.MainUtilities generate-obf ' + mapFile
             print(cmd)
             os.system(cmd)
-            
+
         os.chdir(currentDir)
         return 1
 
@@ -237,18 +266,19 @@ def osmand():
     except:
         print("Unexpected error:", sys.exc_info()[0])
         return 0
-    
+
+
 def mapsme():
-    os.chdir(mapsmeDir + '/omim/tools/python')
+    os.chdir(os.path.join(mapsmeDir, 'omim/tools/python')) 
     os.system(
         'python3.6 -m maps_generator --countries="Belarus*" --skip="coastline"')
     os.chdir(currentDir)
+
 
 def garmin():
     os.chdir(garminDir)
     os.system('bash build.sh')
     os.chdir(currentDir)
-
 
 
 def main():
@@ -260,7 +290,6 @@ def main():
     move()
     clean()
 
+
 if __name__ == '__main__':
     main()
-
-
