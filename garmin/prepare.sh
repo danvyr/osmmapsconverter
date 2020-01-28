72d0f9706fad16c53bee620b211254a2e5426d90
Vitali Danilovich <danvyr@gmail.com>
2020-01-28 09:11:03 +0100

    fix garmin prepare

--- garmin/prepare.sh
+++ garmin/prepare.sh
@@ -9,16 +9,16 @@ wget http://www.mkgmap.org.uk/download/mkgmap-$MKGMAP_VER.tar.gz
 mkdir mkgmap split temp
 
 tar -xf mkgmap-$MKGMAP_VER.tar.gz --directory mkgmap
-mv mkgmap/mkgmap-$MKGMAP_VER/* mkgmap
-rmdir mkgmap/mkgmap-$MKGMAP_VER
+mv mkgmap/mkgmap-*/* mkgmap
+rmdir mkgmap/mkgmap-*
 rm mkgmap-$MKGMAP_VER.tar.gz
 
 
 wget http://www.mkgmap.org.uk/download/splitter-$SPLITTER_VER.tar.gz
 
 tar -xf splitter-$SPLITTER_VER.tar.gz  --directory  split
-mv split/splitter-$SPLITTER_VER/* split
-rmdir split/splitter-$SPLITTER_VER
+mv split/splitter-*/* split
+rmdir split/splitter-*
 rm splitter-$SPLITTER_VER.tar.gz
 
 
