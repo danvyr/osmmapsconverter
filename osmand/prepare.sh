9d496e3a0fdf2fc99fecbb31345e37a4448b0384
Vitali Danilovich <danvyr@gmail.com>
2021-01-02 23:31:30 +0300

    add prepare for osmand

---
+++ osmand/prepare.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf OsmAndMapCreator-main.zip OsmAndMapCreator
+wget https://download.osmand.net/latest-night-build/OsmAndMapCreator-main.zip
+unzip OsmAndMapCreator-main.zip -d OsmAndMapCreator
